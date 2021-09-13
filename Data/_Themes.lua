482945629170160a4a906c4c3a5f4b5f43071e90
Syiana <oliversergej@gmail.com>
2021-09-14 01:34:38 +0200

    fixed default theme

--- Data/_Themes.lua
+++ Data/_Themes.lua
@@ -1,10 +1,8 @@
 local Themes = SUI:NewModule('Data.Themes');
 
-local Class = RAID_CLASS_COLORS[select(2,UnitClass("player"))]
-
 Themes.data = {
   { value = nil, text = 'Blizzard' },
-  { value = {.3, .3, .3}, text = 'Dark' },
-  { value = {Class.r, Class.g, Class.b}, text = 'Class' },
+  { value = 'Dark', text = 'Dark' },
+  { value = 'Class', text = 'Class' },
   { value = 'Custom', text = 'Custom' }
 }
