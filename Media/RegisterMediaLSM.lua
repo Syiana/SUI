cb2fea5ea9b72965b3879f50fe967d83243cb273
Baine <Baine@users.noreply.github.com>
2021-10-31 14:02:52 +0100

    Add SUI Font/Texture to LSM

---
+++ Media/RegisterMediaLSM.lua
@@ -0,0 +1,25 @@
+if IsAddOnLoaded("SharedMedia") then
+	local LSM = LibStub("LibSharedMedia-3.0") 
+
+	-- ----- 
+	-- BACKGROUND 
+	-- ----- 
+
+	-- ----- 
+	--  BORDER 
+	-- ---- 
+
+	-- -----
+	--   FONT
+	-- -----
+	LSM:Register("font", "SUI", [[Interface\Addons\SUI\Media\Fonts\Prototype.ttf]]) 
+
+	-- -----
+	--   SOUND
+	-- -----
+
+	-- -----
+	--   STATUSBAR
+	-- -----
+	LSM:Register("statusbar", "SUI", [[Interface\Addons\SUI\Media\Textures\Status\Smooth.blp]]) 
+end
