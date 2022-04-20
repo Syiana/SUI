859baa997ae421d1702164ca271399f2d2815092
Syiana <suiaddon@gmail.com>
2022-04-21 00:52:19 +0200

    repair

--- Modules/General/_Repair.lua
+++ Modules/General/_Repair.lua
@@ -6,16 +6,6 @@ function Module:OnEnable()
     local g = CreateFrame("Frame")
     g:RegisterEvent("MERCHANT_SHOW")
     g:SetScript("OnEvent", function()
-      local bag, slot
-      for bag = 0, 4 do
-        for slot = 0, GetContainerNumSlots(bag) do
-          local link = GetContainerItemLink(bag, slot)
-          if link and (select(3, GetItemInfo(link)) == 0) then
-            UseContainerItem(bag, slot)
-          end
-        end
-      end
-
       if (CanMerchantRepair()) then
         local cost = GetRepairAllCost()
         if cost > 0 then
@@ -41,22 +31,5 @@ function Module:OnEnable()
         end
       end
     end)
-
-    local NEW_ITEM_VENDOR_STACK_BUY = ITEM_VENDOR_STACK_BUY
-    ITEM_VENDOR_STACK_BUY = "|cffa9ff00" .. NEW_ITEM_VENDOR_STACK_BUY .. "|r"
-
-    local origMerchantItemButton_OnModifiedClick = _G.MerchantItemButton_OnModifiedClick
-    local function MerchantItemButton_OnModifiedClickHook(self, ...)
-      origMerchantItemButton_OnModifiedClick(self, ...)
-      if (IsAltKeyDown()) then
-        local maxStack = select(8, GetItemInfo(GetMerchantItemLink(self:GetID())))
-        local _, _, _, quantity = GetMerchantItemInfo(self:GetID())
-
-        if (maxStack and maxStack > 1) then
-          BuyMerchantItem(self:GetID(), floor(maxStack / quantity))
-        end
-      end
-    end
-    MerchantItemButton_OnModifiedClick = MerchantItemButton_OnModifiedClickHook
   end
 end
