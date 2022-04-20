09278e90999cd286af95210e703a8026d002cc7a
Syiana <suiaddon@gmail.com>
2022-04-20 22:49:43 +0200

    combaticon on unitframes

--- Modules/UnitFrames/_Combat.lua
+++ Modules/UnitFrames/_Combat.lua
@@ -1,55 +1,48 @@
---[[SUI COMBAT v1.0]]
+local Module = SUI:NewModule("UnitFrames.Combat");
 
-local CF=CreateFrame("Frame")
-CF:RegisterEvent("PLAYER_LOGIN")
-CF:SetScript("OnEvent", function(self, event)
+function Module:OnEnable()
+  local db = SUI.db.profile.unitframes.combaticon
 
-if not SUIDB.MODULES.INCOMBAT == true then return end 
+  if (db) then
+    CTT = CreateFrame("Frame")
+    CTT:SetPoint("CENTER", TargetFrame, "RIGHT", -15, 0)
+    CTT:SetSize(25, 25)
+    CTT.t = CTT:CreateTexture(nil, BORDER)
+    CTT.t:SetAllPoints()
+    CTT.t:SetTexture("Interface\\Icons\\ABILITY_DUALWIELD")
+    CTT:Hide()
 
-CTT = CreateFrame("Frame")
-CTT:SetPoint("Right", TargetFrame, 0)
-CTT:SetSize(30, 30)
-CTT.t = CTT:CreateTexture(nil, BORDER)
-CTT.t:SetAllPoints()
-CTT.t:SetTexture("Interface\\Icons\\ABILITY_DUALWIELD")
-CTT:Hide()
-
-local function FrameOnUpdate(self)
-    if UnitAffectingCombat("target") then
+    local function FrameOnUpdate(self)
+      if UnitAffectingCombat("target") then
         self:Show()
-    else
+      else
         self:Hide()
+      end
     end
+
+    local g = CreateFrame("Frame")
+    g:SetScript("OnUpdate", function(self)
+      FrameOnUpdate(CTT)
+    end)
+    CFT = CreateFrame("Frame")
+    CFT:SetPoint("CENTER", FocusFrame, "RIGHT", -15, 0)
+    CFT:SetSize(25, 25)
+    CFT.t = CFT:CreateTexture(nil, BORDER)
+    CFT.t:SetAllPoints()
+    CFT.t:SetTexture("Interface\\Icons\\ABILITY_DUALWIELD")
+    CFT:Hide()
+
+    local function FrameOnUpdate(self)
+      if UnitAffectingCombat("focus") then
+          self:Show()
+      else
+          self:Hide()
+      end
+    end
+
+    local g = CreateFrame("Frame")
+    g:SetScript("OnUpdate", function(self)
+      FrameOnUpdate(CFT)
+    end)
+  end
 end
-local g = CreateFrame("Frame")
-g:SetScript(
-    "OnUpdate",
-    function(self)
-        FrameOnUpdate(CTT)
-    end
-)
-
-CFT = CreateFrame("Frame")
-CFT:SetPoint("Right", FocusFrame, 0)
-CFT:SetSize(30, 30)
-CFT.t = CFT:CreateTexture(nil, BORDER)
-CFT.t:SetAllPoints()
-CFT.t:SetTexture("Interface\\Icons\\ABILITY_DUALWIELD")
-CFT:Hide()
-
-local function FrameOnUpdate(self)
-    if UnitAffectingCombat("focus") then
-        self:Show()
-    else
-        self:Hide()
-    end
-end
-local g = CreateFrame("Frame")
-g:SetScript(
-    "OnUpdate",
-    function(self)
-        FrameOnUpdate(CFT)
-    end
-)
-
-end)
