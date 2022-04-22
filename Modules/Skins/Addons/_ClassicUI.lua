e66b8a9f289d570c0ab1cead47f9349b2a0dc8d2
Syiana <suiaddon@gmail.com>
2022-04-22 19:24:23 +0200

    classicUI skin

---
+++ Modules/Skins/Addons/_ClassicUI.lua
@@ -0,0 +1,16 @@
+local Skin = SUI:NewModule("Skins.ClassicUI.Background");
+
+function Skin:OnEnable()
+  local ClassicUI = IsAddOnLoaded("ClassicUI")
+  if not (ClassicUI) then return end
+  if (SUI:Color()) then
+    print('classicUI')
+    for i, v in pairs({
+      MainMenuBarArtFrameBackground.BackgroundLarge2,
+      MainMenuBarArtFrameBackground.BagsArt,
+      MainMenuBarArtFrameBackground.MicroButtonArt,
+    }) do
+      v:SetVertexColor(unpack(SUI:Color(0.15)))
+    end
+  end
+end
