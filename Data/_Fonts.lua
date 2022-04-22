1614b9ef8a58e3156dbf82567775cf860efd688f
Syiana <suiaddon@gmail.com>
2022-04-22 18:42:19 +0200

    add fonts

--- Data/_Fonts.lua
+++ Data/_Fonts.lua
@@ -3,4 +3,5 @@ local Fonts = SUI:NewModule('Data.Fonts');
 Fonts.data = {
   { value = 'Default' , text = 'Default' },
   { value = 'Interface\\AddOns\\SUI\\Media\\Fonts\\Prototype.ttf', text = 'Prototype' },
+  { value = 'Interface\\AddOns\\SUI\\Media\\Fonts\\Arial_Bold.ttf', text = 'Arial Bold' }
 }
