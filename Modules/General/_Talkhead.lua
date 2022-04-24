2083c698de9c501b5c6886f4b01da60fd8bd8a5d
Syiana <suiaddon@gmail.com>
2022-04-24 15:24:21 +0200

    fix talkhead

--- Modules/General/_Talkhead.lua
+++ Modules/General/_Talkhead.lua
@@ -2,11 +2,15 @@ local Module = SUI:NewModule("General.Talkhead");
 
 function Module:OnEnable()
   if not (SUI.db.profile.general.cosmetic.talkhead) then
-    if addon == "Blizzard_TalkingHeadUI" then
-      hooksecurefunc("TalkingHeadFrame_PlayCurrent", function()
-        TalkingHeadFrame:Hide()
-      end)
+    local TalkHead = CreateFrame("Frame")
+    TalkHead:RegisterEvent("ADDON_LOADED")
+    TalkHead:SetScript("OnEvent", function(self, event, addon)
+      if addon == "Blizzard_TalkingHeadUI" then
+        hooksecurefunc("TalkingHeadFrame_PlayCurrent", function()
+          TalkingHeadFrame_CloseImmediately()
+        end)
       self:UnregisterEvent(event)
-    end
+      end
+    end)
   end
 end
