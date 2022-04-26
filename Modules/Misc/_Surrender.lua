e33e653d9cf61a44f23bce6df683340d31b75001
cleanlock <leifm1996@gmail.com>
2022-04-26 19:22:40 +0200

    Removed namespace stuff from surrender

--- Modules/Misc/_Surrender.lua
+++ Modules/Misc/_Surrender.lua
@@ -3,14 +3,15 @@ local Module = SUI:NewModule("Misc.Surrender");
 function Module:OnEnable()
   local db = SUI.db.profile.misc.surrender
   if (db) then
-    local _, core = ...
     function core:OnLoadCommandHandler (event, name)
-      SLASH_SURRENDERGG1 = "/gg"
-      SlashCmdList.SURRENDERGG = SurrenderArena;
+        --on load assign commands
+        SLASH_SURRENDERGG1 = "/gg"
+        SlashCmdList.SURRENDERGG = SurrenderArena;
 
-      SLASH_SURRENDERSR1 = "/sr"
-      SlashCmdList.SURRENDERSR = SurrenderArena;
+        SLASH_SURRENDERSR1 = "/sr"
+        SlashCmdList.SURRENDERSR = SurrenderArena;
     end
+
     local events = CreateFrame("Frame")
     events:RegisterEvent("ADDON_LOADED")
     events:SetScript("OnEvent",core.OnLoadCommandHandler)
