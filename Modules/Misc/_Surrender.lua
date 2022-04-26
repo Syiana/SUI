987c2a403e9ea1f56da29713b99f44d3ed32ad3d
cleanlock <leifm1996@gmail.com>
2022-04-26 19:34:11 +0200

    Fixed surrender errors

--- Modules/Misc/_Surrender.lua
+++ Modules/Misc/_Surrender.lua
@@ -3,17 +3,16 @@ local Module = SUI:NewModule("Misc.Surrender");
 function Module:OnEnable()
   local db = SUI.db.profile.misc.surrender
   if (db) then
-    function core:OnLoadCommandHandler (event, name)
-        --on load assign commands
-        SLASH_SURRENDERGG1 = "/gg"
-        SlashCmdList.SURRENDERGG = SurrenderArena;
+    function surrender(event, name)
+      SLASH_SURRENDERGG1 = "/gg"
+      SlashCmdList.SURRENDERGG = SurrenderArena;
 
-        SLASH_SURRENDERSR1 = "/sr"
-        SlashCmdList.SURRENDERSR = SurrenderArena;
+      SLASH_SURRENDERSR1 = "/sr"
+      SlashCmdList.SURRENDERSR = SurrenderArena;
     end
 
     local events = CreateFrame("Frame")
     events:RegisterEvent("ADDON_LOADED")
-    events:SetScript("OnEvent",core.OnLoadCommandHandler)
+    events:SetScript("OnEvent", surrender)
   end
 end
