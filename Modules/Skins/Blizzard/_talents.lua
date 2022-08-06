9944cd15ae062d7b063d87de1fd7629c53c73e08
muleyo <leifm1996@gmail.com>
2022-08-06 02:51:20 +0200

    Fixed Talents-Frame themeing

--- Modules/Skins/Blizzard/_talents.lua
+++ Modules/Skins/Blizzard/_talents.lua
@@ -7,78 +7,64 @@ function Module:OnEnable()
     f:SetScript("OnEvent", function(self, event, name)
       if name == "Blizzard_TalentUI" then
         for i, v in pairs({
-          PlayerTalentFrame.NineSlice.TopEdge,
-          PlayerTalentFrame.NineSlice.RightEdge,
-          PlayerTalentFrame.NineSlice.BottomEdge,
-          PlayerTalentFrame.NineSlice.LeftEdge,
-          PlayerTalentFrame.NineSlice.TopRightCorner,
-          PlayerTalentFrame.NineSlice.TopLeftCorner,
-          PlayerTalentFrame.NineSlice.BottomLeftCorner,
-          PlayerTalentFrame.NineSlice.BottomRightCorner,
-          PlayerTalentFrame.NineSlice.BottomEdge,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentList.NineSlice.TopEdge,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentList.NineSlice.RightEdge,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentList.NineSlice.BottomEdge,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentList.NineSlice.LeftEdge,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentList.NineSlice.TopRightCorner,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentList.NineSlice.TopLeftCorner,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentList.NineSlice.BottomLeftCorner,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentList.NineSlice.BottomRightCorner,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentList.NineSlice.BottomEdge, }) do
+          PlayerTalentFrameTopEdge,
+          PlayerTalentFrameRightEdge,
+          PlayerTalentFrameBottomEdge,
+          PlayerTalentFrameLeftEdge,
+          PlayerTalentFrameTopRightCorner,
+          PlayerTalentFrameTopLeftCorner,
+          PlayerTalentFrameBottomLeftCorner,
+          PlayerTalentFrameBottomRightCorner,
+          PlayerTalentFrameBottomEdge,
+          PlayerTalentFrameTalentsPvpTalentFrameTalentListTopEdge,
+          PlayerTalentFrameTalentsPvpTalentFrameTalentListRightEdge,
+          PlayerTalentFrameTalentsPvpTalentFrameTalentListBottomEdge,
+          PlayerTalentFrameTalentsPvpTalentFrameTalentListLeftEdge,
+          PlayerTalentFrameTalentsPvpTalentFrameTalentListTopRightCorner,
+          PlayerTalentFrameTalentsPvpTalentFrameTalentListTopLeftCorner,
+          PlayerTalentFrameTalentsPvpTalentFrameTalentListBottomLeftCorner,
+          PlayerTalentFrameTalentsPvpTalentFrameTalentListBottomRightCorner,
+          PlayerTalentFrameTalentsPvpTalentFrameTalentListBottomEdge, }) do
             v:SetVertexColor(.15, .15, .15)
         end
         for i, v in pairs({
-        PlayerTalentFrameInset.NineSlice.RightEdge,
-        PlayerTalentFrameInset.NineSlice.BottomEdge,
-        PlayerTalentFrameInset.NineSlice.LeftEdge,
-        PlayerTalentFrameInset.NineSlice.TopRightCorner,
-        PlayerTalentFrameInset.NineSlice.TopLeftCorner,
-        PlayerTalentFrameInset.NineSlice.BottomLeftCorner,
-        PlayerTalentFrameInset.NineSlice.BottomRightCorner,
-        PlayerTalentFrameInset.NineSlice.BottomEdge,
-        PlayerTalentFrameTalentsPvpTalentFrameTalentList.Inset.NineSlice.TopEdge,
-        PlayerTalentFrameTalentsPvpTalentFrameTalentList.Inset.NineSlice.RightEdge,
-        PlayerTalentFrameTalentsPvpTalentFrameTalentList.Inset.NineSlice.BottomEdge,
-        PlayerTalentFrameTalentsPvpTalentFrameTalentList.Inset.NineSlice.LeftEdge,
-        PlayerTalentFrameTalentsPvpTalentFrameTalentList.Inset.NineSlice.TopRightCorner,
-        PlayerTalentFrameTalentsPvpTalentFrameTalentList.Inset.NineSlice.TopLeftCorner,
-        PlayerTalentFrameTalentsPvpTalentFrameTalentList.Inset.NineSlice.BottomLeftCorner,
-        PlayerTalentFrameTalentsPvpTalentFrameTalentList.Inset.NineSlice.BottomRightCorner,
-        PlayerTalentFrameTalentsPvpTalentFrameTalentList.Inset.NineSlice.BottomEdge, }) do
+        PlayerTalentFrameInsetRightEdge,
+        PlayerTalentFrameInsetBottomEdge,
+        PlayerTalentFrameInsetLeftEdge,
+        PlayerTalentFrameInsetTopRightCorner,
+        PlayerTalentFrameInsetTopLeftCorner,
+        PlayerTalentFrameInsetBottomLeftCorner,
+        PlayerTalentFrameInsetBottomRightCorner,
+        PlayerTalentFrameInsetBottomEdge,
+        PlayerTalentFrameTalentsPvpTalentFrameTalentListInsetTopEdge,
+        PlayerTalentFrameTalentsPvpTalentFrameTalentListInsetRightEdge,
+        PlayerTalentFrameTalentsPvpTalentFrameTalentListInsetBottomEdge,
+        PlayerTalentFrameTalentsPvpTalentFrameTalentListInsetLeftEdge,
+        PlayerTalentFrameTalentsPvpTalentFrameTalentListInsetTopRightCorner,
+        PlayerTalentFrameTalentsPvpTalentFrameTalentListInsetTopLeftCorner,
+        PlayerTalentFrameTalentsPvpTalentFrameTalentListInsetBottomLeftCorner,
+        PlayerTalentFrameTalentsPvpTalentFrameTalentListInsetBottomRightCorner,
+        PlayerTalentFrameTalentsPvpTalentFrameTalentListInsetBottomEdge, }) do
           v:SetVertexColor(.3, .3, .3)
-        end
-        for i, v in pairs({
-          PlayerTalentFrameTalentsPvpTalentFrameTalentListScrollFrameScrollBarThumbTexture,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentListScrollFrameScrollBarTop,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentListScrollFrameScrollBarMiddle,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentListScrollFrameScrollBarBottom,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentListScrollFrameScrollBarScrollUpButton.Normal,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentListScrollFrameScrollBarScrollDownButton.Normal,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentListScrollFrameScrollBarScrollUpButton.Disabled,
-          PlayerTalentFrameTalentsPvpTalentFrameTalentListScrollFrameScrollBarScrollDownButton.Disabled }) do
-          v:SetVertexColor(.4, .4, .4)
         end
         for i, v in pairs({
           PVEFrameTopFiligree,
           PVEFrameBottomFiligree,
           PVEFrameBlueBg,
-          LFGListFrame.CategorySelection.Inset.CustomBG }) do
-            v:SetVertexColor(.5, .5, .5)
+        }) do
+        v:SetVertexColor(.5, .5, .5)
         end
         for i, v in pairs({
-          PlayerTalentFrame.Bg,
-          PlayerTalentFrame.TitleBg,
-          PlayerTalentFrameInset.Bg,
+          PlayerTalentFrameBg,
+          PlayerTalentFrameTitleBg,
+          PlayerTalentFrameInsetBg,
           PlayerTalentFrameTalentsPvpTalentFrameTalentListBg }) do
             v:SetVertexColor(.3, .3, .3)
         end
         for i, v in pairs({
-          PlayerTalentFrameTalents.bg }) do
+          PlayerTalentFrameTalentsbg }) do
             v:SetVertexColor(.5, .5, .5)
         end
-        _G.select(1, PlayerTalentFrameSpecialization:GetRegions()):SetVertexColor(.4, .4, .4)
-        _G.select(5, PlayerTalentFrameSpecialization:GetRegions()):SetVertexColor(.5, .5, .5)
-        _G.select(6, PlayerTalentFrameSpecialization:GetRegions()):SetVertexColor(.5, .5, .5)
       end
     end)
   end
