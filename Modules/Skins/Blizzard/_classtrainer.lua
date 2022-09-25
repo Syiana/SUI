d987685a111bd361dc4f5c4bc37fc154937f92e2
muleyo <leifm1996@gmail.com>
2022-09-25 21:45:26 +0200

    Fixed several small errors

--- Modules/Skins/Blizzard/_classtrainer.lua
+++ Modules/Skins/Blizzard/_classtrainer.lua
@@ -6,14 +6,14 @@ function Module:OnEnable()
     f:RegisterEvent("ADDON_LOADED")
     f:SetScript("OnEvent", function(self, event, name)
       if name == "Blizzard_TrainerUI" then
-        for i, v in pairs({ ClassTrainerFrame.NineSlice.TopEdge,
-          ClassTrainerFrame.NineSlice.RightEdge,
-          ClassTrainerFrame.NineSlice.BottomEdge,
-          ClassTrainerFrame.NineSlice.LeftEdge,
-          ClassTrainerFrame.NineSlice.TopRightCorner,
-          ClassTrainerFrame.NineSlice.TopLeftCorner,
-          ClassTrainerFrame.NineSlice.BottomLeftCorner,
-          ClassTrainerFrame.NineSlice.BottomRightCorner }) do
+        for i, v in pairs({ ClassTrainerFrameTopEdge,
+          ClassTrainerFrameRightEdge,
+          ClassTrainerFrameBottomEdge,
+          ClassTrainerFrameLeftEdge,
+          ClassTrainerFrameTopRightCorner,
+          ClassTrainerFrameTopLeftCorner,
+          ClassTrainerFrameBottomLeftCorner,
+          ClassTrainerFrameBottomRightCorner }) do
             v:SetVertexColor(.15, .15, .15)
         end
         for i, v in pairs({
