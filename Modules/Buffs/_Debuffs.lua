2e143d4833f14a38e58907cdbef32a6a8b0466a3
muleyo <leifm1996@gmail.com>
2022-11-06 21:49:06 +0100

    Updated Debuffs

--- Modules/Buffs/_Debuffs.lua
+++ Modules/Buffs/_Debuffs.lua
@@ -1,4 +1,123 @@
-local Debuffs = SUI:NewModule("Buffs.Debuffs");
+local Debuffs = SUI:NewModule("Buffs.Debuffs")
+
+function Debuffs:OnEnable()
+    local hooked = {}
+
+    -- DebuffType Colors for the Debuff Border
+    debuffColor = {}
+    debuffColor["none"]    = { r = 0.80, g = 0, b = 0 };
+    debuffColor["Magic"]   = { r = 0.20, g = 0.60, b = 1.00 };
+    debuffColor["Curse"]   = { r = 0.60, g = 0.00, b = 1.00 };
+    debuffColor["Disease"] = { r = 0.60, g = 0.40, b = 0 };
+    debuffColor["Poison"]  = { r = 0.00, g = 0.60, b = 0 };
+
+    function UpdateDebuffStyling(pool)
+        for frame, _ in pairs(pool.activeObjects) do
+            if not hooked[frame] then
+                hooked[frame] = true
+
+                local icon = frame.Icon
+                icon:SetTexCoord(0.1, 0.9, 0.1, 0.9)
+                if frame.Border then frame.Border:Hide() end
+                frame.duration:SetFont(STANDARD_TEXT_FONT, 10, "OUTLINE")
+
+                if DebuffFrame.exampleAuraFrames == nil then
+                    hooksecurefunc(frame, "UpdateDuration", function(self)
+                        
+                        local durationText = self.duration:GetText()
+                        if durationText ~= nil then
+                            if strfind(durationText, " ") then
+                                self.duration:SetText(gsub(durationText, " ", ""))
+                            end
+                        end
+
+                        local count = self.count
+                        if count ~= nil then
+                            count:SetFont(STANDARD_TEXT_FONT, 11, "OUTLINE")
+                            count:ClearAllPoints()
+                            count:SetPoint("TOPRIGHT", self, "TOPRIGHT", 0, -2)
+                        end
+
+                        self.duration:ClearAllPoints()
+                        self.duration:SetPoint("CENTER", frame, "BOTTOM", 0, 15)
+                        self.duration:SetFont(STANDARD_TEXT_FONT, 12, "OUTLINE")
+                    end)
+                end
+                
+                ButtonBackdrop(frame)
+            end
+        end
+    end
+    
+    for _, pool in pairs(DebuffFrame.auraPool.pools) do
+        hooksecurefunc(pool, "Acquire", UpdateDebuffStyling)
+    end
+
+    EditModeManagerFrame:HookScript("OnShow", function()
+        hooksecurefunc(DebuffFrame, "UpdateAuraButtons", function() end)
+    end)
+
+    function ButtonDefault(button)
+        local Backdrop = {
+            bgFile = "",
+            edgeFile = "Interface\\Addons\\SUI\\Media\\Textures\\Core\\outer_shadow",
+            tile = false,
+            tileSize = 32,
+            edgeSize = 5,
+            insets = { left = 5, right = 5, top = 5, bottom = 5 }
+        }
+        local icon = button.Icon
+
+        local border = CreateFrame("Frame", nil, button)
+        border:SetSize(icon:GetWidth() + 5, icon:GetHeight() + 6)
+        border:SetPoint("CENTER", button, "CENTER", 1, 5)
+
+        border.texture = border:CreateTexture(nil, "Border")
+        border.texture:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\Core\\UIActionBar")
+        border.texture:SetTexCoord(0.701171875, 0.880859375, 0.31689453125, 0.36083984375)
+        border.texture:SetVertexColor(0, 0, 0)
+        border.texture:SetAllPoints()
+
+        local shadow = CreateFrame("Frame", nil, border, "BackdropTemplate")
+        shadow:SetSize(icon:GetWidth(), icon:GetHeight())
+        shadow:SetPoint("TOPLEFT", border, "TOPLEFT", -2, 2)
+        shadow:SetPoint("BOTTOMRIGHT", border, "BOTTOMRIGHT", 1, -1)
+        shadow:SetFrameLevel(button:GetFrameLevel() - 1)
+        shadow:SetBackdrop(Backdrop)
+        shadow:SetBackdropBorderColor(0, 0, 0)
+    end
+
+    function ButtonBackdrop(button)
+        local Backdrop = {
+            bgFile = "",
+            edgeFile = "Interface\\Addons\\SUI\\Media\\Textures\\Core\\outer_shadow",
+            tile = false,
+            tileSize = 32,
+            edgeSize = 5,
+            insets = { left = 5, right = 5, top = 5, bottom = 5 }
+        }
+
+        local icon = button.Icon
+        local point, relativeTo, relativePoint, xOfs, yOfs = icon:GetPoint()
+
+        local border = CreateFrame("Frame", nil, button)
+        border:SetSize(icon:GetWidth(), icon:GetHeight())
+        border:SetPoint("CENTER", button, "CENTER", 0, 0)
+
+        border.texture = border:CreateTexture(nil, "Border")
+        border.texture:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\Core\\Normal_N")
+        border.texture:SetVertexColor(0, 0, 0)
+        border.texture:SetSize(40, 40)
+        border.texture:SetPoint(point, relativeTo, relativePoint, xOfs, yOfs + 5)
+    end
+
+    function ButtonBorder(Button)
+
+    end
+end
+
+
+--[[local Debuffs = SUI:NewModule("Buffs.Debuffs");
 
 function Debuffs:OnEnable()
   if IsAddOnLoaded("BlizzBuffsFacade") then return end
@@ -93,4 +212,4 @@ function Debuffs:OnEnable()
   frame:SetScript("OnEvent", function(self, event, ...)
     updateDebuffs()
   end)
-end
+end]]
