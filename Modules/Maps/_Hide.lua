272fc37c7c4e125e95c906f5ad72687b645c7e54
muleyo <leifm1996@gmail.com>
2022-11-25 23:45:42 +0100

    Added check if SexyMap is loaded for Map Options

--- Modules/Maps/_Hide.lua
+++ Modules/Maps/_Hide.lua
@@ -2,27 +2,28 @@ local Module = SUI:NewModule("Maps.Hide");
 
 function Module:OnEnable()
     local db = SUI.db.profile.maps
-    if not (db.date) then
-        GameTimeFrame:Hide()
-        GameTimeFrame:UnregisterAllEvents()
-        GameTimeFrame.Show = kill
-    end
-    if not (db.tracking) then
-        MinimapCluster.Tracking.Button:Hide()
-        MinimapCluster.Tracking.Background:Hide()
-        MinimapCluster.Tracking.Button:UnregisterAllEvents()
-    end
-    if not (db.clock) then
-        TimeManagerClockButton:Hide()
-        TimeManagerClockTicker:Hide()
-    end
+    if not (IsAddOnLoaded("SexyMap")) then
+        if not db.date then
+            GameTimeFrame:UnregisterAllEvents()
+            GameTimeFrame:Hide()
+        end
+        if not db.tracking then
+            MinimapCluster.Tracking.Button:UnregisterAllEvents()
+            MinimapCluster.Tracking.Button:Hide()
+            MinimapCluster.Tracking.Background:Hide()
+        end
+        if not db.clock then
+            TimeManagerClockButton:Hide()
+            TimeManagerClockTicker:Hide()
+        end
 
-    local Hide = CreateFrame("Frame")
-    Hide:RegisterEvent("ADDON_LOADED")
-    Hide:RegisterEvent("PLAYER_LOGIN")
-    Hide:RegisterEvent("PLAYER_ENTERING_WORLD")
-    Hide:RegisterEvent("VARIABLES_LOADED")
-    Hide:SetScript("OnEvent", function()
-        MinimapCluster.BorderTop:Hide(0)
-    end)
+        local Hide = CreateFrame("Frame")
+        Hide:RegisterEvent("ADDON_LOADED")
+        Hide:RegisterEvent("PLAYER_LOGIN")
+        Hide:RegisterEvent("PLAYER_ENTERING_WORLD")
+        Hide:RegisterEvent("VARIABLES_LOADED")
+        Hide:SetScript("OnEvent", function()
+            MinimapCluster.BorderTop:Hide(0)
+        end)
+    end
 end
