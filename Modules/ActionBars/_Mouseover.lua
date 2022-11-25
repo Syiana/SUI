604f59e2d4dace51d171f779d83a60fc0d4d6ef7
muleyo <leifm1996@gmail.com>
2022-11-25 03:16:43 +0100

    Update Mouseover GCD Flash

--- Modules/ActionBars/_Mouseover.lua
+++ Modules/ActionBars/_Mouseover.lua
@@ -4,9 +4,6 @@ function Module:OnEnable()
     if IsAddOnLoaded('Bartender4') or IsAddOnLoaded('Dominos') then return end
     local db = SUI.db.profile.actionbar.bars
 
-    local gcd = CreateFrame("Frame")
-    gcd:RegisterEvent("SPELL_UPDATE_COOLDOWN")
-
     local function hideGCD(button)
         for i=1,12 do
             if _G[button .. i .. "Cooldown"] then
@@ -23,16 +20,93 @@ function Module:OnEnable()
         end
     end
 
-    if db.bar1 then
-        -- Hide MainMenuBar
-        MainMenuBar:SetAlpha(0)
-        gcd:HookScript("OnEvent", function()
+    local gcd = CreateFrame("Frame")
+    gcd:RegisterEvent("SPELL_UPDATE_COOLDOWN")
+    gcd:HookScript("OnEvent", function()
+        if db.bar1 then
             if MainMenuBar:GetEffectiveAlpha() and MainMenuBar:GetEffectiveAlpha() > 0.001 then
                 showGCD("ActionButton")
             else
                 hideGCD("ActionButton")
             end
-        end)
+        end
+
+        if db.bar2 then
+            if MultiBarBottomLeft:GetEffectiveAlpha() and MultiBarBottomLeft:GetEffectiveAlpha() > 0.001 then
+                showGCD("MultiBarBottomLeftButton")
+            else
+                hideGCD("MultiBarBottomLeftButton")
+            end
+        end
+
+        if db.bar3 then
+            if MultiBarBottomRight:GetEffectiveAlpha() and MultiBarBottomRight:GetEffectiveAlpha() > 0.001 then
+                showGCD("MultiBarBottomRightButton")
+            else
+                hideGCD("MultiBarBottomRightButton")
+            end
+        end
+
+        if db.bar4 then
+            if MultiBarRight:GetEffectiveAlpha() and MultiBarRight:GetEffectiveAlpha() > 0.001 then
+                showGCD("MultiBarRightButton")
+            else
+                hideGCD("MultiBarRightButton")
+            end
+        end
+
+        if db.bar5 then
+            if MultiBarLeft:GetEffectiveAlpha() and MultiBarLeft:GetEffectiveAlpha() > 0.001 then
+                showGCD("MultiBarLeftButton")
+            else
+                hideGCD("MultiBarLeftButton")
+            end
+        end
+
+        if db.bar6 then
+            if MultiBar5:GetEffectiveAlpha() and MultiBar5:GetEffectiveAlpha() > 0.001 then
+                showGCD("MultiBar5Button")
+            else
+                hideGCD("MultiBar5Button")
+            end
+        end
+
+        if db.bar7 then
+            if MultiBar6:GetEffectiveAlpha() and MultiBar6:GetEffectiveAlpha() > 0.001 then
+                showGCD("MultiBar6Button")
+            else
+                hideGCD("MultiBar6Button")
+            end
+        end
+
+        if db.bar8 then
+            if MultiBar7:GetEffectiveAlpha() and MultiBar7:GetEffectiveAlpha() > 0.001 then
+                showGCD("MultiBar7Button")
+            else
+                hideGCD("MultiBar7Button")
+            end
+        end
+
+        if db.petbar then
+            if PetActionBar:GetEffectiveAlpha() and PetActionBar:GetEffectiveAlpha() > 0.001 then
+                showGCD("PetActionButton")
+            else
+                hideGCD("PetActionButton")
+            end
+        end
+
+        if db.stancebar then
+            if StanceBar:GetEffectiveAlpha() and StanceBar:GetEffectiveAlpha() > 0.001 then
+                showGCD("StanceButton")
+            else
+                hideGCD("StanceButton")
+            end
+        end
+    end)
+
+    if db.bar1 then
+        -- Hide MainMenuBar
+        MainMenuBar:SetAlpha(0)
 
         -- Show MainMenuBar on Hover
         for i=1,12 do
@@ -49,13 +123,6 @@ function Module:OnEnable()
     if db.bar2 then
         -- Hide MultiBarBottomLeft
         MultiBarBottomLeft:SetAlpha(0)
-        gcd:HookScript("OnEvent", function()
-            if MultiBarBottomLeft:GetEffectiveAlpha() and MultiBarBottomLeft:GetEffectiveAlpha() > 0.001 then
-                showGCD("MultiBarBottomLeftButton")
-            else
-                hideGCD("MultiBarBottomLeftButton")
-            end
-        end)
 
         -- Show MultiBarBottomLeft on Hover
         for i=1,12 do
@@ -72,13 +139,6 @@ function Module:OnEnable()
     if db.bar3 then
         -- Hide MultiBarBottomRight
         MultiBarBottomRight:SetAlpha(0)
-        gcd:HookScript("OnEvent", function()
-            if MultiBarBottomRight:GetEffectiveAlpha() and MultiBarBottomRight:GetEffectiveAlpha() > 0.001 then
-                showGCD("MultiBarBottomRightButton")
-            else
-                hideGCD("MultiBarBottomRightButton")
-            end
-        end)
 
         -- Show MultiBarBottomRight on Hover
         for i=1,12 do
@@ -96,14 +156,6 @@ function Module:OnEnable()
         -- Hide MultiBarRight
         MultiBarRight:SetAlpha(0)
 
-        gcd:HookScript("OnEvent", function()
-            if MultiBarRight:GetEffectiveAlpha() and MultiBarRight:GetEffectiveAlpha() > 0.001 then
-                showGCD("MultiBarRightButton")
-            else
-                hideGCD("MultiBarRightButton")
-            end
-        end)
-
         -- Show MultiBarRight on Hover
         for i=1,12 do
             _G["MultiBarRightButton" ..i]:HookScript('OnEnter', function(self)
@@ -119,13 +171,6 @@ function Module:OnEnable()
     if db.bar5 then
         -- Hide MultiBarLeft
         MultiBarLeft:SetAlpha(0)
-        gcd:HookScript("OnEvent", function()
-            if MultiBarLeft:GetEffectiveAlpha() and MultiBarLeft:GetEffectiveAlpha() > 0.001 then
-                showGCD("MultiBarLeftButton")
-            else
-                hideGCD("MultiBarLeftButton")
-            end
-        end)
 
         -- Show MultiBarLeft on Hover
         for i=1,12 do
@@ -142,13 +187,6 @@ function Module:OnEnable()
     if db.bar6 then
         -- Hide MultiBar5
         MultiBar5:SetAlpha(0)
-        gcd:HookScript("OnEvent", function()
-            if MultiBar5:GetEffectiveAlpha() and MultiBar5:GetEffectiveAlpha() > 0.001 then
-                showGCD("MultiBar5Button")
-            else
-                hideGCD("MultiBar5Button")
-            end
-        end)
 
         -- Show MultiBar5 on Hover
         for i=1,12 do
@@ -165,13 +203,6 @@ function Module:OnEnable()
     if db.bar7 then
         -- Hide MultiBar6
         MultiBar6:SetAlpha(0)
-        gcd:HookScript("OnEvent", function()
-            if MultiBar6:GetEffectiveAlpha() and MultiBar6:GetEffectiveAlpha() > 0.001 then
-                showGCD("MultiBar6Button")
-            else
-                hideGCD("MultiBar6Button")
-            end
-        end)
 
         -- Show MultiBar6 on Hover
         for i=1,12 do
@@ -188,13 +219,6 @@ function Module:OnEnable()
     if db.bar8 then
         -- Hide MultiBar7
         MultiBar7:SetAlpha(0)
-        gcd:HookScript("OnEvent", function()
-            if MultiBar7:GetEffectiveAlpha() and MultiBar7:GetEffectiveAlpha() > 0.001 then
-                showGCD("MultiBar7Button")
-            else
-                hideGCD("MultiBar7Button")
-            end
-        end)
 
         -- Show MultiBar7 on Hover
         for i=1,12 do
@@ -211,13 +235,6 @@ function Module:OnEnable()
     if db.petbar then
         -- Hide Pet Actionbar
         PetActionBar:SetAlpha(0)
-        gcd:HookScript("OnEvent", function()
-            if PetActionBar:GetEffectiveAlpha() and PetActionBar:GetEffectiveAlpha() > 0.001 then
-                showGCD("PetActionButton")
-            else
-                hideGCD("PetActionButton")
-            end
-        end)
 
         -- Show Pet Actionbar on Hover
         for i=1,10 do
@@ -234,13 +251,6 @@ function Module:OnEnable()
     if db.stancebar then
         -- Hide Stancebar
         StanceBar:SetAlpha(0)
-        gcd:HookScript("OnEvent", function()
-            if StanceBar:GetEffectiveAlpha() and StanceBar:GetEffectiveAlpha() > 0.001 then
-                showGCD("StanceButton")
-            else
-                hideGCD("StanceButton")
-            end
-        end)
 
         -- Show Stancebar on Hover
         for i=1,8 do
