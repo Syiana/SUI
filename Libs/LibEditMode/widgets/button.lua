6a7c115473a8ee25eaad448cd606f1dfee62c8aa
muleyo <leifm1996@gmail.com>
2022-11-30 01:21:31 +0100

    Update libs

--- Libs/LibEditMode/widgets/button.lua
+++
@@ -1,6 +0,0 @@
-local lib = LibStub('LibEditMode')
-local internal = lib.internal
-
-internal:CreatePool('button', function()
-	return CreateFrame('Button', nil, UIParent, 'EditModeSystemSettingsDialogExtraButtonTemplate')
-end)
