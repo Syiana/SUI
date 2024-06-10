344461d2b814de2632f7341b0453057d6ddce9f0
cleanlock <admin@muleyo.dev>
2024-06-10 21:29:34 +0200

    Fixed AddonList Styling

--- Modules/Skins/Blizzard/_addonlist.lua
+++ Modules/Skins/Blizzard/_addonlist.lua
@@ -3,44 +3,37 @@ local Module = SUI:NewModule("Skins.AddonList");
 function Module:OnEnable()
   if (SUI:Color()) then
     for i, v in pairs({
-      AddonListTopEdge,
-      AddonListRightEdge,
-      AddonListBottomEdge,
-      AddonListLeftEdge,
-      AddonListTopRightCorner,
-      AddonListTopLeftCorner,
-      AddonListBottomLeftCorner,
-      AddonListBottomRightCorner, }) do
-        v:SetVertexColor(.15, .15, .15)
-    end
-    for i, v in pairs({
-      AddonListInsetTopEdge,
-      AddonListInsetTopRightCorner,
-      AddonListInsetRightEdge,
-      AddonListInsetBottomRightCorner,
-      AddonListInsetBottomEdge,
-      AddonListInsetBottomLeftCorner,
-      AddonListInsetLeftEdge,
-      AddonListInsetTopLeftCorner, }) do
-        v:SetVertexColor(.3, .3, .3)
-    end
-    for i, v in pairs({
       AddonListBg,
-      AddonList.TitleBg, }) do
-        v:SetVertexColor(.3, .3, .3)
-    end
-    for i, v in pairs({
+      AddonList.TitleBg,
+      AddonListTopBorder,
+      AddonListRightBorder,
+      AddonListLeftBorder,
+      AddonListBottomBorder,
+      AddonListTopLeftCorner,
+      AddonListTopRightCorner,
+      AddonListBotLeftCorner,
+      AddonListBotRightCorner,
+      AddonListBtnCornerLeft,
+      AddonListBtnCornerRight,
+      AddonListInsetInsetLeftBorder,
+      AddonListInsetInsetRightBorder,
+      AddonListInsetInsetTopBorder,
+      AddonListInsetInsetBottomBorder,
+      AddonListInsetInsetTopLeftCorner,
+      AddonListInsetInsetTopRightCorner,
+      AddonListInsetInsetBotLeftCorner,
+      AddonListInsetInsetBotRightCorner,
       AddonListScrollFrameScrollBarTop,
       AddonListScrollFrameScrollBarMiddle,
       AddonListScrollFrameScrollBarBottom,
-      AddonListScrollFrameScrollBarThumbTexture,
-      AddonListScrollFrameScrollBarScrollUpButton.Normal,
-      AddonListScrollFrameScrollBarScrollDownButton.Normal,
-      AddonListScrollFrameScrollBarScrollUpButton.Disabled,
-      AddonListScrollFrameScrollBarScrollDownButton.Disabled,
-
+      AddonListBg,
+      AddonListButtonBottomBorder,
+      AddonListEnableAllButton_RightSeparator,
+      AddonListDisableAllButton_RightSeparator,
+      AddonListOkayButton_LeftSeparator,
+      AddonListCancelButton_LeftSeparator
     }) do
-      v:SetVertexColor(.4, .4, .4)
+        v:SetVertexColor(.15, .15, .15)
     end
   end
 end
