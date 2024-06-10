ecc137225ba766bc86b59a7b66ed83490885d969
cleanlock <admin@muleyo.dev>
2024-06-10 21:19:17 +0200

    Fixed Paladin ClassBar Styling

--- Modules/Skins/Blizzard/_unitframes.lua
+++ Modules/Skins/Blizzard/_unitframes.lua
@@ -3,20 +3,12 @@ local Module = SUI:NewModule("Skins.UnitFrames");
 function Module:OnEnable()
   if (SUI:Color()) then
     for i, v in ipairs({
+      PaladinPowerBarBG,
       PlayerFrameTexture,
       TargetFrameTextureFrameTexture,
       PlayerFrameAlternateManaBarBorder,
       PlayerFrameAlternateManaBarLeftBorder,
       PlayerFrameAlternateManaBarRightBorder,
-      PaladinPowerBarFrameBG,
-      PaladinPowerBarFrameBankBG,
-      -- ComboPointPlayerFrame.Background,
-      -- ComboPointPlayerFrame.Combo1.PointOff,
-      -- ComboPointPlayerFrame.Combo2.PointOff,
-      -- ComboPointPlayerFrame.Combo3.PointOff,
-      -- ComboPointPlayerFrame.Combo4.PointOff,
-      -- ComboPointPlayerFrame.Combo5.PointOff,
-      -- ComboPointPlayerFrame.Combo6.PointOff,
       PetFrameTexture,
       PartyMemberFrame1Texture,
       PartyMemberFrame2Texture,
@@ -39,29 +31,8 @@ function Module:OnEnable()
       Boss3TargetFrameSpellBar.Border,
       Boss4TargetFrameSpellBar.Border,
       Boss5TargetFrameSpellBar.Border,
-      -- StatusTrackingBarManager.SingleBarLargeUpper,
-      -- StatusTrackingBarManager.SingleBarSmallUpper,
     }) do
       v:SetVertexColor(unpack(SUI:Color(0.15)))
     end
-
-    -- SUI:SetScript("OnEvent", function(self, event)
-    --   ColorRaid()
-    --   PlayerFrameGroupIndicator:SetAlpha(0)
-    --   PlayerHitIndicator:SetText(nil)
-    --   PlayerHitIndicator.SetText = function()
-    --   end
-    --   PetHitIndicator:SetText(nil)
-    --   PetHitIndicator.SetText = function()
-    --   end
-    --   for _, child in pairs({WarlockPowerFrame:GetChildren()}) do
-    --     for _, region in pairs({child:GetRegions()}) do
-    --       if region:GetDrawLayer() == "BORDER" then
-    --         region:SetVertexColor(unpack(color.secondary))
-    --       end
-    --     end
-    --   end
-
-    -- end)
   end
 end
