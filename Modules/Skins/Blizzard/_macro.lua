83e7870d34a4744ae56c8582ff89bd3b1a688bd2
cleanlock <admin@muleyo.dev>
2024-06-11 19:51:13 +0200

    Fixed MacroMenu Style

--- Modules/Skins/Blizzard/_macro.lua
+++ Modules/Skins/Blizzard/_macro.lua
@@ -1,53 +1,22 @@
 local Module = SUI:NewModule("Skins.Macro");
 
 function Module:OnEnable()
-  if (SUI:Color()) then
-    local f = CreateFrame("Frame")
-    f:RegisterEvent("ADDON_LOADED")
-    f:SetScript("OnEvent", function(self, event, name)
-      if name == "Blizzard_MacroUI" then
-        for i, v in pairs({ MacroFrame.NineSlice.TopEdge,
-          MacroFrame.NineSlice.RightEdge,
-          MacroFrame.NineSlice.BottomEdge,
-          MacroFrame.NineSlice.LeftEdge,
-          MacroFrame.NineSlice.TopRightCorner,
-          MacroFrame.NineSlice.TopLeftCorner,
-          MacroFrame.NineSlice.BottomLeftCorner,
-          MacroFrame.NineSlice.BottomRightCorner }) do
-            v:SetVertexColor(unpack(SUI:Color(0.15)))
-        end
-        for i, v in pairs({
-          MacroFrameInset.NineSlice.TopEdge,
-          MacroFrameInset.NineSlice.TopRightCorner,
-          MacroFrameInset.NineSlice.RightEdge,
-          MacroFrameInset.NineSlice.BottomRightCorner,
-          MacroFrameInset.NineSlice.BottomEdge,
-          MacroFrameInset.NineSlice.BottomLeftCorner,
-          MacroFrameInset.NineSlice.LeftEdge,
-          MacroFrameInset.NineSlice.TopLeftCorner, }) do
-            v:SetVertexColor(unpack(SUI:Color()))
-        end
-        for i, v in pairs({
-          MacroFrame.Bg,
-          MacroFrame.TitleBg }) do
-            v:SetVertexColor(unpack(SUI:Color()))
-        end
-        for i, v in pairs({
-          MacroButtonScrollFrameTop,
-          MacroButtonScrollFrameMiddle,
-          MacroButtonScrollFrameBottom,
-          MacroButtonScrollFrameScrollBarThumbTexture,
-          MacroButtonScrollFrameScrollBarScrollUpButton.Normal,
-          MacroButtonScrollFrameScrollBarScrollDownButton.Normal,
-          MacroButtonScrollFrameScrollBarScrollUpButton.Disabled,
-          MacroButtonScrollFrameScrollBarScrollDownButton.Disabled,
-          }) do
-          v:SetVertexColor(unpack(SUI:Color()))
-        end
-        MacroHorizontalBarLeft:Hide();
-        --fix
-        --_G.select(2, MacroFrame:GetRegions()):Hide()
-      end
-    end)
-  end
+    if (SUI:Color()) then
+        local f = CreateFrame("Frame")
+        f:RegisterEvent("ADDON_LOADED")
+        f:SetScript("OnEvent", function(self, event, name)
+            if name == "Blizzard_MacroUI" then
+                SUI:Skin(MacroFrame)
+                SUI:Skin(MacroFrameInset)
+                SUI:Skin(MacroDeleteButton)
+                SUI:Skin(MacroNewButton)
+                SUI:Skin(MacroExitButton)
+                SUI:Skin(MacroFrameTab1)
+                SUI:Skin(MacroFrameTab2)
+                SUI:Skin(MacroFrame.MacroSelector.ScrollBar.Background)
+                SUI:Skin(MacroFrameTextBackground.NineSlice)
+                SUI:Skin(MacroFrameTextBackground)
+            end
+        end)
+    end
 end
