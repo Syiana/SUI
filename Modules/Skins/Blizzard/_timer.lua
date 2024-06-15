cff4ab6ba85d311b15fdbc287c8d44a76396af87
cleanlock <admin@muleyo.dev>
2024-06-15 23:40:18 +0200

    fIXES

--- Modules/Skins/Blizzard/_timer.lua
+++ Modules/Skins/Blizzard/_timer.lua
@@ -6,19 +6,9 @@ function Module:OnEnable()
 
         TimerTracker:HookScript("OnEvent", function(self, event, timerType, timeSeconds, totalTime)
             for i = 1, #self.timerList do
-                local timer = _G['TimerTrackerTimer' .. i]
                 local border = _G['TimerTrackerTimer' .. i .. 'StatusBarBorder']
-                local statusbar = _G['TimerTrackerTimer' .. i .. 'StatusBar']
 
                 border:SetVertexColor(unpack(SUI:Color()))
-
-                if (db.texture ~= 'Default') then
-                    statusbar:SetStatusBarTexture(db.texture)
-                    statusbar:GetStatusBarTexture():SetDrawLayer("BORDER")
-                else
-                    statusbar:SetStatusBarTexture("Interface\\Addons\\SUI\\Media\\Textures\\Unitframes\\UI-StatusBar")
-                    statusbar:GetStatusBarTexture():SetDrawLayer("BORDER")
-                end
             end
         end)
         for _, region in pairs({ StopwatchFrame:GetRegions() }) do
