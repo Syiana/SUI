52d5dc7474dc4664c7bc2f2cac42e9958c00bc45
cleanlock <admin@muleyo.dev>
2024-06-18 01:01:21 +0200

    Fix DressUpFrame Style

--- Modules/Skins/Blizzard/_dressup.lua
+++ Modules/Skins/Blizzard/_dressup.lua
@@ -2,35 +2,8 @@ local Module = SUI:NewModule("Skins.Dressup");
 
 function Module:OnEnable()
     if (SUI:Color()) then
-        for i, v in pairs({
-            DressUpFrameTopEdge,
-            DressUpFrameRightEdge,
-            DressUpFrameBottomEdge,
-            DressUpFrameLeftEdge,
-            DressUpFrameTopRightCorner,
-            DressUpFrameTopLeftCorner,
-            DressUpFrameBottomLeftCorner,
-            DressUpFrameBottomRightCorner,
-            DressUpFrameInsetBottomEdge,
-        }) do
-            v:SetVertexColor(.15, .15, .15)
-        end
-        for i, v in pairs({
-            DressUpFrameBg,
-            DressUpFrameTitleBg,
-            DressUpFrameInsetBg }) do
-            v:SetVertexColor(.3, .3, .3)
-        end
-        for i, v in pairs({
-            DressUpFrameInsetTopEdge,
-            DressUpFrameInsetTopRightCorner,
-            DressUpFrameInsetRightEdge,
-            DressUpFrameInsetBottomRightCorner,
-            DressUpFrameInsetBottomEdge,
-            DressUpFrameInsetBottomLeftCorner,
-            DressUpFrameInsetLeftEdge,
-            DressUpFrameInsetTopLeftCorner }) do
-            v:SetVertexColor(.3, .3, .3)
-        end
+        SUI:Skin(DressUpFrame)
+        SUI:Skin(DressUpFrameResetButton)
+        SUI:Skin(DressUpFrameCancelButton)
     end
 end
