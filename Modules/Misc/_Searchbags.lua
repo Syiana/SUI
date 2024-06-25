c6b73c5f79a6e734b42db54cb228021f86a13bf0
cleanlock <admin@muleyo.dev>
2024-06-25 10:17:40 +0200

    Added Bags Searchbox

---
+++ Modules/Misc/_Searchbags.lua
@@ -0,0 +1,30 @@
+local Module = SUI:NewModule("Misc.Searchbags");
+
+function Module:OnEnable()
+    if
+        IsAddOnLoaded("Bagnon")
+        or IsAddOnLoaded("AdiBags")
+        or IsAddOnLoaded("ArkInventory")
+        or IsAddOnLoaded("Baggins")
+        or IsAddOnLoaded("Inventorian")
+    then
+        return
+    end
+
+    local db = SUI.db.profile.misc.searchbags
+    if (db) then
+        hooksecurefunc("ContainerFrame_Update", function( self )
+            if self:GetID() == 0 then
+                BagItemSearchBox:SetParent(self)
+                BagItemSearchBox:SetPoint("TOPLEFT", self, "TOPLEFT", 55, -29)
+                BagItemSearchBox:SetWidth(75)
+                BagItemSearchBox.anchorBag = self
+                BagItemSearchBox:Show()
+            elseif BagItemSearchBox.anchorBag == self then
+                BagItemSearchBox:ClearAllPoints()
+                BagItemSearchBox:Hide()
+                BagItemSearchBox.anchorBag = nil
+            end
+        end)
+    end
+end
