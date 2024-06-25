b0f29f26207a0b61f2fe439c0293e52697f0fa79
cleanlock <admin@muleyo.dev>
2024-06-25 19:01:13 +0200

    Fixed some CharacterFrame styling

--- Modules/Skins/Blizzard/_character.lua
+++ Modules/Skins/Blizzard/_character.lua
@@ -25,12 +25,46 @@ function Module:OnEnable()
         SUI:Skin(CharacterSecondaryHandSlot)
         SUI:Skin(CharacterRangedSlot)
         SUI:Skin(CharacterStatsPane.ScrollBar.Track)
-        SUI:Skin(CharacterStatsPane.ScrollBar.Track.Thumb)
         SUI:Skin(CharacterFrameInsetRight)
         SUI:Skin(CharacterFrameTab1)
         SUI:Skin(CharacterFrameTab3)
         SUI:Skin(CharacterFrameTab4)
         SUI:Skin(ReputationListScrollFrame)
+        SUI:Skin({
+            PaperDollInnerBorderTop,
+            PaperDollInnerBorderLeft,
+            PaperDollInnerBorderRight,
+            PaperDollInnerBorderBottom,
+            PaperDollInnerBorderBottom2,
+            PaperDollInnerBorderTopLeft,
+            PaperDollInnerBorderTopRight,
+            PaperDollInnerBorderBottomLeft,
+            PaperDollInnerBorderBottomRight
+        }, false, true)
+
+        CharacterFrame:HookScript("OnUpdate", function()
+            SUI:Skin({
+                CharacterHeadSlotNormalTexture,
+                CharacterNeckSlotNormalTexture,
+                CharacterShoulderSlotNormalTexture,
+                CharacterBackSlotNormalTexture,
+                CharacterChestSlotNormalTexture,
+                CharacterShirtSlotNormalTexture,
+                CharacterTabardSlotNormalTexture,
+                CharacterWristSlotNormalTexture,
+                CharacterHandsSlotNormalTexture,
+                CharacterWaistSlotNormalTexture,
+                CharacterLegsSlotNormalTexture,
+                CharacterFeetSlotNormalTexture,
+                CharacterFinger0SlotNormalTexture,
+                CharacterFinger1SlotNormalTexture,
+                CharacterTrinket0SlotNormalTexture,
+                CharacterTrinket1SlotNormalTexture,
+                CharacterMainHandSlotNormalTexture,
+                CharacterSecondaryHandSlotNormalTexture,
+                CharacterRangedSlotNormalTexture
+            }, false, true)
+        end)
 
         local f = CreateFrame("Frame")
         f:RegisterEvent("ADDON_LOADED")
