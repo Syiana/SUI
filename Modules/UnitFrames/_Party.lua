5b2c60306d27d4b49cef7a6136cd8f47fd02d0b0
cleanlock <admin@muleyo.dev>
2024-06-26 21:38:46 +0200

    Fixed PartyFrame Textures

--- Modules/UnitFrames/_Party.lua
+++ Modules/UnitFrames/_Party.lua
@@ -6,20 +6,18 @@ function Module:OnEnable()
         texture = SUI.db.profile.general.texture
     }
 
-    if (db.unitframes.style == 'Big') then
-        function SUIPartyFrames()
-            local useCompact = GetCVarBool("useCompactPartyFrames");
-            if IsInGroup(player) and (not IsInRaid(player)) and (not useCompact) then
-                for i = 1, 4 do
-                    if (db.texture ~= 'Default') then
-                        _G["PartyMemberFrame" .. i .. "HealthBar"]:SetStatusBarTexture(db.texture);
-                        _G["PartyMemberFrame" .. i .. "ManaBar"]:SetStatusBarTexture(db.texture);
-                    end
+    function SUIPartyFrames()
+        local useCompact = GetCVarBool("useCompactPartyFrames");
+        if IsInGroup() and (not IsInRaid()) and (not useCompact) then
+            for i = 1, 4 do
+                if (db.texture ~= 'Default') then
+                    _G["PartyMemberFrame" .. i .. "HealthBar"]:SetStatusBarTexture(db.texture);
+                    _G["PartyMemberFrame" .. i .. "ManaBar"]:SetStatusBarTexture(db.texture);
+                end
+                if (db.unitframes.style == 'Big') then
                     _G["PartyMemberFrame" .. i .. "Name"]:SetSize(75, 10);
-                    _G["PartyMemberFrame" .. i .. "Texture"]:SetTexture(
-                    "Interface\\Addons\\SUI\\Media\\Textures\\unitframes\\UI-PartyFrame");
-                    _G["PartyMemberFrame" .. i .. "Flash"]:SetTexture(
-                    "Interface\\Addons\\SUI\\Media\\Textures\\unitframes\\UI-PARTYFRAME-FLASH");
+                    _G["PartyMemberFrame" .. i .. "Texture"]:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\unitframes\\UI-PartyFrame");
+                    _G["PartyMemberFrame" .. i .. "Flash"]:SetTexture("Interface\\Addons\\SUI\\Media\\Textures\\unitframes\\UI-PARTYFRAME-FLASH");
                     _G["PartyMemberFrame" .. i .. "HealthBar"]:ClearAllPoints();
                     _G["PartyMemberFrame" .. i .. "HealthBar"]:SetPoint("TOPLEFT", 45, -13);
                     _G["PartyMemberFrame" .. i .. "HealthBar"]:SetHeight(12);
@@ -27,37 +25,22 @@ function Module:OnEnable()
                     _G["PartyMemberFrame" .. i .. "ManaBar"]:SetPoint("TOPLEFT", 45, -26);
                     _G["PartyMemberFrame" .. i .. "ManaBar"]:SetHeight(5);
                     _G["PartyMemberFrame" .. i .. "HealthBarTextLeft"]:ClearAllPoints();
-                    _G["PartyMemberFrame" .. i .. "HealthBarTextLeft"]:SetPoint("LEFT",
-                        _G["PartyMemberFrame" .. i .. "HealthBar"], "LEFT", 0, 0);
+                    _G["PartyMemberFrame" .. i .. "HealthBarTextLeft"]:SetPoint("LEFT", _G["PartyMemberFrame" .. i .. "HealthBar"], "LEFT", 0, 0);
                     _G["PartyMemberFrame" .. i .. "HealthBarTextRight"]:ClearAllPoints();
-                    _G["PartyMemberFrame" .. i .. "HealthBarTextRight"]:SetPoint("RIGHT",
-                        _G["PartyMemberFrame" .. i .. "HealthBar"], "RIGHT", 0, 0);
+                    _G["PartyMemberFrame" .. i .. "HealthBarTextRight"]:SetPoint("RIGHT", _G["PartyMemberFrame" .. i .. "HealthBar"], "RIGHT", 0, 0);
                     _G["PartyMemberFrame" .. i .. "ManaBarTextLeft"]:ClearAllPoints();
-                    _G["PartyMemberFrame" .. i .. "ManaBarTextLeft"]:SetPoint("LEFT",
-                        _G["PartyMemberFrame" .. i .. "ManaBar"], "LEFT", 0, 0);
+                    _G["PartyMemberFrame" .. i .. "ManaBarTextLeft"]:SetPoint("LEFT", _G["PartyMemberFrame" .. i .. "ManaBar"], "LEFT", 0, 0);
                     _G["PartyMemberFrame" .. i .. "ManaBarTextRight"]:ClearAllPoints();
-                    _G["PartyMemberFrame" .. i .. "ManaBarTextRight"]:SetPoint("RIGHT",
-                        _G["PartyMemberFrame" .. i .. "ManaBar"], "RIGHT", 0, 0);
+                    _G["PartyMemberFrame" .. i .. "ManaBarTextRight"]:SetPoint("RIGHT", _G["PartyMemberFrame" .. i .. "ManaBar"], "RIGHT", 0, 0);
                     _G["PartyMemberFrame" .. i .. "HealthBarText"]:ClearAllPoints();
-                    _G["PartyMemberFrame" .. i .. "HealthBarText"]:SetPoint("CENTER",
-                        _G["PartyMemberFrame" .. i .. "HealthBar"], "CENTER", 0, 0);
+                    _G["PartyMemberFrame" .. i .. "HealthBarText"]:SetPoint("CENTER", _G["PartyMemberFrame" .. i .. "HealthBar"], "CENTER", 0, 0);
                     _G["PartyMemberFrame" .. i .. "ManaBarText"]:ClearAllPoints();
-                    _G["PartyMemberFrame" .. i .. "ManaBarText"]:SetPoint("CENTER", _G["PartyMemberFrame" .. i .. "ManaBar"],
-                        "CENTER", 0, 0);
+                    _G["PartyMemberFrame" .. i .. "ManaBarText"]:SetPoint("CENTER", _G["PartyMemberFrame" .. i .. "ManaBar"], "CENTER", 0, 0);
                 end
             end
         end
-
-        function SUIPartyText()
-            for i = 1, 4 do
-                -- _G["PartyMemberFrame"..i.."ManaBarText"]:SetText(" ");
-                -- _G["PartyMemberFrame"..i.."ManaBarTextLeft"]:SetText(" ");
-                -- _G["PartyMemberFrame"..i.."ManaBarTextRight"]:SetText(" ");
-            end
-        end
-
-        hooksecurefunc("UnitFrame_Update", SUIPartyFrames)
-        hooksecurefunc("PartyMemberFrame_ToPlayerArt", SUIPartyFrames)
-        hooksecurefunc("TextStatusBar_UpdateTextStringWithValues", SUIPartyText)
     end
+
+    hooksecurefunc("UnitFrame_Update", SUIPartyFrames)
+    hooksecurefunc("PartyMemberFrame_ToPlayerArt", SUIPartyFrames)
 end
