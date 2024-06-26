0e408ae9e5809a00c24017909f23b84037ea11a3
cleanlock <admin@muleyo.dev>
2024-06-26 20:04:12 +0200

    Fixed Bank Container Styling

--- Modules/Skins/Blizzard/_Bags.lua
+++ Modules/Skins/Blizzard/_Bags.lua
@@ -2,7 +2,7 @@ local Module = SUI:NewModule("Skins.Bags");
 
 function Module:OnEnable()
     if (SUI:Color()) then
-        for i = 1, 5 do
+        for i = 1, 12 do
             SUI:Skin(_G["ContainerFrame" .. i])
         end
 
