5f7295e2b899f1a11c9d630b5f4394bbb64710df
muleyo <admin@muleyo.dev>
2024-07-12 11:31:13 +0200

    Fixed RuneIcons Color in CharacterFrame

--- Core/Init.lua
+++ Core/Init.lua
@@ -272,17 +272,19 @@ function SUI:OnInitialize()
             ["LootFramePortraitOverlay"] = true, ["CalendarCreateEventIcon"] = true,
             ["StaticPopup1AlertIcon"] = true, ["PVPConquestFrameInfoButtonInfoIconAlliance"] = true,
             ["PVPConquestFrameInfoButtonInfoIconHorde"] = true, ["ContainerFrame1Portrait"] = true,
+            ["CharacterHeadSlotSubIconTexture"] = true, ["CharacterShouldersSlotSubIconTexture"] = true,
+            ["CharacterNeckSlotSubIconTexture"] = true, ["CharacterBackSlotSubIconTexture"] = true,
+            ["CharacterChestSlotSubIconTexture"] = true, ["CharacterWristSlotSubIconTexture"] = true,
+            ["CharacterHandsSlotSubIconTexture"] = true, ["CharacterWaistSlotSubIconTexture"] = true,
+            ["CharacterLegsSlotSubIconTexture"] = true, ["CharacterFeetSlotSubIconTexture"] = true,
+            ["CharacterFinger0SlotSubIconTexture"] = true, ["CharacterFinger1SlotSubIconTexture"] = true,
             [select(1,QuestLogFrame:GetRegions())] = true, [select(18, MailFrame:GetRegions())] = true,
             [select(2, SpellBookSkillLineTab1:GetRegions())] = true, [select(2, SpellBookSkillLineTab2:GetRegions())] = true,
             [select(2, SpellBookSkillLineTab3:GetRegions())] = true, [select(2, SpellBookSkillLineTab4:GetRegions())] = true,
             [select(1, ItemTextFrame:GetRegions())] = true, [select(6, SendMailMoneyGold:GetRegions())] = true,
             [select(6, SendMailMoneySilver:GetRegions())] = true, [select(6, SendMailMoneyCopper:GetRegions())] = true,
             [select(18, ChannelFrame:GetRegions())] = true, [select(1, SpellBookFrame:GetRegions())] = true,
-            [select(2, QuestLogFrame:GetRegions())] = true, [select(7, CharacterHeadSlot:GetRegions())] = true,
-            [select(7, CharacterBackSlot:GetRegions())] = true, [select(7, CharacterChestSlot:GetRegions())] = true,
-            [select(7, CharacterWristSlot:GetRegions())] = true, [select(7, CharacterHandsSlot:GetRegions())] = true,
-            [select(7, CharacterWaistSlot:GetRegions())] = true, [select(7, CharacterLegsSlot:GetRegions())] = true,
-            [select(7, CharacterFeetSlot:GetRegions())] = true
+            [select(2, QuestLogFrame:GetRegions())] = true,
         }
 
         if (frame) then
