8e3c8a63da21054e0311794f6219be1b4868a98f
muleyo <admin@muleyo.dev>
2024-09-17 10:38:49 +0200

    Fixed an error when Blizzard theme was selected

--- Modules/Skins/Blizzard/_dragonriding.lua
+++ Modules/Skins/Blizzard/_dragonriding.lua
@@ -1,26 +1,28 @@
 local Module = SUI:NewModule("Skins.Dragonriding");
 
 function Module:OnEnable()
-    local function skinVigor()
-        for _, child in ipairs({UIWidgetPowerBarContainerFrame:GetChildren()}) do
-            for _, v in ipairs({child:GetRegions()}) do
-                if v:GetObjectType() == "Texture" then
-                    v:SetVertexColor(unpack(SUI:Color(0.15)))
-                end
-            end
-            for _, vigor in ipairs({child:GetChildren()}) do
-                for _, v in ipairs({vigor:GetRegions()}) do
+    if (SUI:Color()) then
+        local function skinVigor()
+            for _, child in ipairs({UIWidgetPowerBarContainerFrame:GetChildren()}) do
+                for _, v in ipairs({child:GetRegions()}) do
                     if v:GetObjectType() == "Texture" then
                         v:SetVertexColor(unpack(SUI:Color(0.15)))
                     end
                 end
+                for _, vigor in ipairs({child:GetChildren()}) do
+                    for _, v in ipairs({vigor:GetRegions()}) do
+                        if v:GetObjectType() == "Texture" then
+                            v:SetVertexColor(unpack(SUI:Color(0.15)))
+                        end
+                    end
+                end
             end
         end
-    end
 
-    local updateFrame = CreateFrame("Frame")
-    updateFrame:RegisterEvent("UPDATE_UI_WIDGET")
-    updateFrame:HookScript("OnEvent", function()
-        skinVigor()
-    end)
+        local updateFrame = CreateFrame("Frame")
+        updateFrame:RegisterEvent("UPDATE_UI_WIDGET")
+        updateFrame:HookScript("OnEvent", function()
+            skinVigor()
+        end)
+    end
 end
