c0260d5bce0c3e543745b8d32fc925b20f1d4af1
muleyo <admin@muleyo.dev>
2025-02-16 23:08:10 +0100

    Some more styling fixes

--- Core/Init.lua
+++ Core/Init.lua
@@ -382,6 +382,12 @@ function SUI:OnInitialize()
             [select(2, ChatFrame6EditBox:GetRegions())] = true,
             [select(2, ChatFrame7EditBox:GetRegions())] = true,
             [select(1, TradeFrame.RecipientOverlay:GetRegions())] = true,
+            ["StaticPopup1AlertIcon"] = true,
+            ["StaticPopup2AlertIcon"] = true,
+            ["StaticPopup3AlertIcon"] = true,
+            ["PVPReadyDialogBackground"] = true,
+            ["LFGDungeonReadyDialogBackground"] = true,
+            [select(4, LFGListInviteDialog:GetRegions())] = true,
         }
 
         if (frame) then
--- Modules/Skins/Blizzard/_frames.lua
+++ Modules/Skins/Blizzard/_frames.lua
@@ -18,5 +18,19 @@ function Module:OnEnable()
         SUI:Skin(ReportFrame.Border, true)
         SUI:Skin(LFGDungeonReadyDialog, true)
         SUI:Skin(LFGDungeonReadyDialog.Border, true)
+        SUI:Skin(PVPMatchScoreboard.Content, true)
+        SUI:Skin(QueueStatusFrame, true)
+        SUI:Skin(QueueStatusFrame.NineSlice, true)
+        SUI:Skin(LFGListInviteDialog, true)
+        SUI:Skin(LFGListInviteDialog.Border, true)
+
+        PVPMatchScoreboard:HookScript("OnShow", function()
+            SUI:Skin(PVPMatchScoreboard, true)
+        end)
+
+        -- Tabs
+        SUI:Skin(PVPScoreboardTab1, true)
+        SUI:Skin(PVPScoreboardTab2, true)
+        SUI:Skin(PVPScoreboardTab3, true)
     end
 end
